942288feac7c530d6677501f8dca8fb8bff09d10
James Cameron <jamiecam43@gmail.com>
2026-06-10 16:45:17 -0400

    did the thing

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,43 +1,111 @@
-import { createContext, useState, useEffect, useContext } from 'react';
+import {
+	createContext,
+	useState,
+	useEffect,
+	useContext,
+	useReducer,
+} from 'react';
 import { BASE_URL } from '../App';
 
 const CitiesContext = createContext();
 
+const initialState = {
+	cities: [],
+	isLoading: false,
+	currentCity: {},
+	error: '',
+};
+
+function reducer(state, action) {
+	switch (action.type) {
+		case 'loading':
+			return {
+				...state,
+				isLoading: true,
+			};
+		case 'cities/loaded':
+			return {
+				...state,
+				isLoading: false,
+				cities: action.payload,
+			};
+
+		case 'city/loaded':
+			return {
+				...state,
+				isLoading: false,
+				currentCity: action.payload,
+			};
+
+		case 'city/created':
+			return {
+				...state,
+				isLoading: false,
+				cities: [...state.cities, action.payload],
+				currentCity: action.payload,
+			};
+
+		case 'city/deleted':
+			return {
+				...state,
+				isLoading: false,
+				cities: state.cities.filter((city) => city.id !== action.payload),
+				currentCity: {},
+			};
+
+		case 'rejected':
+			return {
+				...state,
+				isLoading: false,
+				error: action.payload,
+			};
+
+		default:
+			throw new Error('Unknown action');
+	}
+}
+
 function CitiesProvider({ children }) {
-	const [cities, setCities] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
-	const [currentCity, setCurrentCity] = useState({});
+	const [{ cities, isLoading, currentCity, error }, dispatch] = useReducer(
+		reducer,
+		initialState,
+	);
 
 	useEffect(function () {
 		async function fetchCities() {
-			setIsLoading(true);
+			dispatch({ type: 'loading' });
 			try {
 				const res = await fetch(`${BASE_URL}/cities`);
 				const data = await res.json();
-				setCities(data);
-			} catch (error) {
-			} finally {
-				setIsLoading(false);
+				dispatch({ type: 'cities/loaded', payload: data });
+			} catch {
+				dispatch({
+					type: 'rejected',
+					payload: 'There was an error loading data...',
+				});
 			}
 		}
 		fetchCities();
 	}, []);
 
 	async function getCity(id) {
-		setIsLoading(true);
+		if (id === currentCity.id) return;
+		dispatch({ type: 'loading' });
 		try {
 			const res = await fetch(`${BASE_URL}/cities/${id}`);
 			const data = await res.json();
-			setCurrentCity(data);
-		} catch (error) {
-		} finally {
-			setIsLoading(false);
+			dispatch({ type: 'city/loaded', payload: data });
+		} catch {
+			dispatch({
+				type: 'rejected',
+				payload: 'There was an error loading city data...',
+			});
 		}
 	}
 
 	async function createCity(newCity) {
+		dispatch({ type: 'loading' });
 		try {
-			setIsLoading(true);
 			const res = await fetch(`${BASE_URL}/cities`, {
 				method: 'POST',
 				body: JSON.stringify(newCity),
@@ -46,26 +114,28 @@ function CitiesProvider({ children }) {
 				},
 			});
 			const data = await res.json();
-			setCities((c) => [...c, data]);
-		} catch (error) {
-			alert(`There was an error creating the city`);
-		} finally {
-			setIsLoading(false);
+			dispatch({ type: 'city/created', payload: data });
+		} catch {
+			dispatch({
+				type: 'rejected',
+				payload: 'There was an error creating city...',
+			});
 		}
 	}
 
 	async function deleteCity(id) {
+		dispatch({ type: 'loading' });
 		try {
-			setIsLoading(true);
 			await fetch(`${BASE_URL}/cities/${id}`, {
 				method: 'DELETE',
 			});
 
-			setCities((c) => c.filter((city) => city.id !== id));
-		} catch (error) {
-			alert(`There was an error deleting city`);
-		} finally {
-			setIsLoading(false);
+			dispatch({ type: 'city/deleted', payload: id });
+		} catch {
+			dispatch({
+				type: 'rejected',
+				payload: 'There was an error deleting city...',
+			});
 		}
 	}
 
@@ -78,6 +148,7 @@ function CitiesProvider({ children }) {
 				getCity,
 				createCity,
 				deleteCity,
+				error,
 			}}>
 			{children}
 		</CitiesContext.Provider>
